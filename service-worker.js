0e6b2c70e857a6136481c943de85a6336abed0ef
Angelo Araujo <angelo18@gmail.com>
2017-11-14 13:07:26 -0200

    Removed ambiguos file

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('tabela-v1').then(function(cache) {
         return cache.addAll([
-          '/',
           '/index.html'
         ]);
       })
